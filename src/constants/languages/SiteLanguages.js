fab6e00c3b984dd2fae93ddae3b42a3046621738
sztamas99 <szucs.tamas1999@gmail.com>
2022-08-27 22:15:43 +0200

    changing SiteLanguages.js with comments

--- src/constants/languages/SiteLanguages.js
+++ src/constants/languages/SiteLanguages.js
@@ -1,3 +1,35 @@
+//You must assign one of the following CODES into SiteLanguages object as value to use it:
+
+//CODE	LANGUAGE
+// cs_CZ	Czech (Czech Republic)
+// el_GR	Greek (Greece)
+// pl_PL	Polish (Poland)
+// ro_RO	Romanian (Romania)
+// hu_HU	Hungarian (Hungary) **already created
+// en_GB	English (United Kingdom)
+// de_DE	German (Germany)
+// es_ES	Spanish (Spain)
+// it_IT	Italian (Italy)
+// fr_FR	French (France)
+// ja_JP	Japanese (Japan)
+// ko_KR	Korean (Korea)
+// es_MX	Spanish (Mexico)
+// es_AR	Spanish (Argentina)
+// pt_BR	Portuguese (Brazil)
+// en_US	English (United States) **already created
+// en_AU	English (Australia)
+// ru_RU	Russian (Russia)
+// tr_TR	Turkish (Turkey)
+// ms_MY	Malay (Malaysia)
+// en_PH	English (Republic of the Philippines)
+// en_SG	English (Singapore)
+// th_TH	Thai (Thailand)
+// vn_VN	Vietnamese (Viet Nam)
+// id_ID	Indonesian (Indonesia)
+// zh_MY	Chinese (Malaysia)
+// zh_CN	Chinese (China)
+// zh_TW	Chinese (Taiwan)
+
 export const SiteLanguages = {
 
     huLanguage: 'hu_HU',
